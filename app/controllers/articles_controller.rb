42a368fa1338cadabe1ebdd6f443c1f1485621cc
Rails Udemy Course <wenrich85@gmail.com>
2017-01-31 22:27:57 +0000

    add partials and destroy action, linked homepage to blog

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -38,7 +38,12 @@ class ArticlesController < ApplicationController
       @article = Article.find(params[:id])
       
     end
-  
+  def destroy
+    @article = Article.find(params[:id])
+    @article.destroy
+    redirect_to articles_path
+    flash[:notice] = "Article was successfully deleted"
+  end
   
   private
   def article_params
